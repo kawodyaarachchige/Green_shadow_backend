a1b8502c7a7376eab74dda68588bfdf29fe69696
Tharushi Kawodya Arachchige <139765472+kawodyaarachchige@users.noreply.github.com>
2024-11-18 05:22:43 +0530

    implemented AppWideExceptionHandler

--- src/main/java/com/example/green_shadow/advisor/AppWideExceptionHandler.java
+++ src/main/java/com/example/green_shadow/advisor/AppWideExceptionHandler.java
@@ -1,4 +1,34 @@
 package com.example.green_shadow.advisor;
 
+import com.example.green_shadow.exception.InvalidUserRoleException;
+import com.example.green_shadow.exception.NoSuchEntityException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
+
 public class AppWideExceptionHandler {
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<String> handleException(Exception exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler({NoSuchEntityException.class})
+    public ResponseEntity<String> handleException(NoSuchEntityException exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
+    @ExceptionHandler({InvalidUserRoleException.class})
+    public ResponseEntity<String> handleException(InvalidUserRoleException exception) {
+        log.error(exception.getMessage());
+        return ResponseEntity.badRequest().body(exception.getMessage());
+    }
+
 }
+
